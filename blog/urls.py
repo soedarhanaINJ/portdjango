68a41b99fe4f6800587a0d293133892aabf0bf8e
soedarhanaINJ <i.soedarhana@gmail.com>
2023-07-03 23:39:57 +0900

    change4commit

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('about', views.about, name='about'),
     path('contact', views.contact, name='contact'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ] 
